3cdfaa6653ed2f873434b150d2c38d58e8f0fc34
User <User@TECH-018440.OnBase.net>
2019-02-12 19:20:56 -0500

    Did in-class work and challenges!

--- Week02/Program.cs
+++ Week02/Program.cs
@@ -6,8 +6,33 @@ namespace Week02
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
             
+            Console.WriteLine("Hello World");
+            //Hello World
+            string userName = "(za-CH)";
+            int userAge = 15;
+            int numberOfPets = 0;
+            string schoolAttended = "Westlake High School";
+
+            Console.WriteLine("Hello! My name is " + userName);
+            //Hello! My name is (za-CH)
+            Console.WriteLine(String.Format("I am {0} years old and have {1} pets!", userAge, numberOfPets));
+            
+            //In-class work
+            bool randBool = false;
+            double randDouble = 3.14;
+            int randInt = 9001;
+            string randText = "Just some random text!";
+
+            Console.WriteLine($"{randBool}, {randDouble}, {randInt}, {randText}");
+            //Challenges
+            Random rnd = new Random();
+            int randomInt = rnd.Next(26);
+            Console.WriteLine($"{randomInt}");
+            int randomDiceRollOne = rnd.Next(1, 7);
+            int randomDiceRollTwo = rnd.Next(1, 7);
+            Console.WriteLine($"{randomDiceRollOne}, {randomDiceRollTwo}");
         }
+        
     }
 }
