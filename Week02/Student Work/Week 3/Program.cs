557beee044bdd9dddd9650bfc8fee7ba4d837de7
User <User@TECH-018440.OnBase.net>
2019-02-19 19:07:30 -0500

    Added Exercises and Week 3 Folder

--- Week02/Student Work/Week 3/Program.cs
+++ Week02/Student Work/Week 3/Program.cs
@@ -6,7 +6,21 @@ namespace Week_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            System.Console.WriteLine("What grade are you in?");
+            int studentGrade = Convert.ToInt32(Console.ReadLine());
+
+            if (studentGrade <= 5)
+            {
+                Console.WriteLine("You are in Elementary School");
+            } else if (studentGrade <= 8) 
+            {
+                Console.WriteLine("You are in Middle School");
+            } else if (studentGrade <= 12)
+            {
+                Console.WriteLine("You are in High School");
+            } else {
+                Console.WriteLine("You are maybe in college");
+            }
         }
     }
 }
