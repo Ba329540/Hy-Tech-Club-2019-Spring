652f0cf92a43aae7beba2c3d7de23654f80d8a7d
User <User@TECH-018440.OnBase.net>
2019-02-19 19:26:24 -0500

    Last commit of 2/19/2019

---
+++ Week02/Student Work/Week 3 Exercises/Week3Challenges.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace challenges;
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Challenge 1
+            Console.WriteLine("This will tell whether your number is an even or an odd number");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("This is an even number");
+            } else {
+                Console.WriteLine("This is an odd number");
+            }
+
+            //Challenge 2
+            Console.WriteLine()
+
+            //Challenge 3
+            Console.WriteLine("Type in a letter of the alphabet");
+            int letter = 
+
+        }
+    }
+}
